66a2b4a25ff1b19ff886a926468ef070109788d8
krik8235 <krik8235@gmail.com>
2024-12-21 23:54:07 +0800

    minor fix

--- src/versionhq/__init__.py
+++ src/versionhq/__init__.py
@@ -11,7 +11,7 @@ from versionhq.agent.model import Agent
 from versionhq.clients.customer.model import Customer
 from versionhq.clients.product.model import Product, ProductProvider
 from versionhq.clients.workflow.model import MessagingWorkflow
-from versionhq.llm import LLM
+from versionhq.llm.model import LLM
 from versionhq.task.model import Task, TaskOutput
 from versionhq.team.model import Team, TeamOutput
 from versionhq.tool.model import Tool
