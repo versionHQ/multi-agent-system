1b986070c07ab0771452597b579e7e3d4fb83cd3
krik8235 <krik8235@gmail.com>
2025-02-28 01:06:49 +0800

    minor updates

--- src/versionhq/llm/llm_vars.py
+++ src/versionhq/llm/llm_vars.py
@@ -32,39 +32,33 @@ MODELS = {
     ],
     "anthropic": [
         "claude-3-7-sonnet-latest",
-        "claude-3-5-sonnet-20241022",
-        "claude-3-5-sonnet-20240620",
-        "claude-3-haiku-2024030",
-        "claude-3-opus-20240229",
-        "claude-3-haiku-20240307",
+        "claude-3-5-haiku-latest",
+        "claude-3-5-sonnet-latest",
+        "claude-3-opus-latest",
     ],
     "openrouter": [
         "openrouter/deepseek/deepseek-r1",
-
         "openrouter/qwen/qwen-2.5-72b-instruct",
-
         "openrouter/google/gemini-2.0-flash-thinking-exp:free",
         "openrouter/google/gemini-2.0-flash-thinking-exp-1219:free",
         "openrouter/google/gemini-2.0-flash-001",
-
         "openrouter/meta-llama/llama-3.3-70b-instruct",
         "openrouter/mistralai/mistral-large-2411",
         "openrouter/cohere/command-r-plus",
+        "openrouter/databricks/dbrx-instruct",
     ],
     "bedrock": [
         "bedrock/converse/us.meta.llama3-3-70b-instruct-v1:0",
         "bedrock/us.meta.llama3-2-1b-instruct-v1:0",
         "bedrock/us.meta.llama3-2-3b-instruct-v1:0",
         "bedrock/us.meta.llama3-2-11b-instruct-v1:0",
-
+        "bedrock/us.meta.llama3-2-90b-instruct-v1:0",
         "bedrock/mistral.mistral-7b-instruct-v0:2",
         "bedrock/mistral.mixtral-8x7b-instruct-v0:1",
         "bedrock/mistral.mistral-large-2407-v1:0",
-
         "bedrock/amazon.titan-text-lite-v1",
         "bedrock/amazon.titan-text-express-v1",
         "bedrock/amazon.titan-text-premier-v1:0",
-
         "bedrock/cohere.command-r-plus-v1:0",
         "bedrock/cohere.command-r-v1:0",
         "bedrock/cohere.command-text-v14",
