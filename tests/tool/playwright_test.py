75954c4e037cc6053e109bd62e0937e1b62332d1
krik8235 <krik8235@gmail.com>
2025-03-31 21:44:37 +0800

    minor fix

--- tests/tool/playwright_test.py
+++ tests/tool/playwright_test.py
@@ -1,12 +1,12 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_has_title(page: Page):
+def _test_has_title(page: Page):
     page.goto("https://playwright.dev/")
     expect(page).to_have_title(re.compile("Playwright"))
 
 
-def test_get_started_link(page: Page):
+def _test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
     page.get_by_role("link", name="Get started").click()
 
