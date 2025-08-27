04ced3c915476bc763b435e9561b5d797d5f5342
krik8235 <krik8235@gmail.com>
2025-08-28 03:26:00 +0800

    inor fix on readme

--- src/versionhq/__init__.py
+++ src/versionhq/__init__.py
@@ -35,7 +35,7 @@ from versionhq.agent_network.formation import form_agent_network
 from versionhq.task_graph.draft import workflow
 
 
-__version__ = "1.2.4.18"
+__version__ = "1.2.4.19"
 __all__ = [
     "Agent",
 
